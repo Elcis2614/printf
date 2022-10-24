4081bc8f15486975d70270231b81e384ce32e6e8
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-23 23:48:10 -0700

    betty putchar

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 /**
- * -putchar - writes the character c to stdout
- *  @c: the character to write
- *
- *  Return : on success 1
- *  On Error, -1 is returned , and errno is set appropriatly.
+ * _putchar - writes the character c to stdout
+ * @c: the character to write
+ * Return: on success 1
+ * On Error, -1 is returned , and errno is set appropriatly.
  */
 int _putchar(char c)
 {
-	return (write(1,&c, 1));
+	return (write(1, &c, 1));
 }
